38c6a4c574a6becc0158e4dedf01dc88cded10e7
Mark Heath <147668+markheath@users.noreply.github.com>
2008-03-07 14:02:51 +0000

    adding the new files missed from previous checkin

---
+++ SilverNibbles/SilverNibbles/Key.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace SilverNibbles
+{
+	public partial class Key : UserControl
+	{
+		public Key()
+		{
+			// Required to initialize variables
+			InitializeComponent();
+		}
+
+        public string Text
+        {
+            get { return runKey.Text; }
+            set { runKey.Text = value; }
+        }
+	}
+}
