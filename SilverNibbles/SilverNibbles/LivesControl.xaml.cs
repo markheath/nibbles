13d8c6e42a3c64f2fddb2d63e8d572331a14cb94
Mark Heath <147668+markheath@users.noreply.github.com>
2008-11-08 21:37:53 +0000

    using Data Binding and the MVVM pattern for the Scoreboard

--- SilverNibbles/SilverNibbles/LivesControl.xaml.cs
+++ SilverNibbles/SilverNibbles/LivesControl.xaml.cs
@@ -17,6 +17,20 @@ namespace SilverNibbles
             DependencyProperty.Register("Fill", typeof(Brush), typeof(LivesControl),
                         new PropertyMetadata(new PropertyChangedCallback(FillChangedCallback)));
 
+
+
+        public int Lives
+        {
+            get { return (int)GetValue(LivesProperty); }
+            set { SetValue(LivesProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Lives.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LivesProperty =
+            DependencyProperty.Register("Lives", typeof(int), typeof(LivesControl), new PropertyMetadata(0, new PropertyChangedCallback(LivesChangedCallback)));
+
+
+
         private static void FillChangedCallback(DependencyObject obj,
                                 DependencyPropertyChangedEventArgs args)
         {
@@ -26,6 +40,17 @@ namespace SilverNibbles
             livesControl.L3.Fill = livesControl.Fill;
             livesControl.L4.Fill = livesControl.Fill;
             livesControl.L5.Fill = livesControl.Fill;
+        }
+
+        private static void LivesChangedCallback(DependencyObject obj,
+                        DependencyPropertyChangedEventArgs args)
+        {
+            LivesControl livesControl = (LivesControl)obj;
+            livesControl.L1.Visibility = (livesControl.Lives >= 1) ? Visibility.Visible : Visibility.Collapsed;
+            livesControl.L2.Visibility = (livesControl.Lives >= 2) ? Visibility.Visible : Visibility.Collapsed;
+            livesControl.L3.Visibility = (livesControl.Lives >= 3) ? Visibility.Visible : Visibility.Collapsed;
+            livesControl.L4.Visibility = (livesControl.Lives >= 4) ? Visibility.Visible : Visibility.Collapsed;
+            livesControl.L5.Visibility = (livesControl.Lives >= 5) ? Visibility.Visible : Visibility.Collapsed;
         }
 
         public Brush Fill
@@ -46,22 +71,6 @@ namespace SilverNibbles
             InitializeComponent();
         }
 
-        int lives;
-
-        public int Lives
-        {
-            get { return lives; }
-            set
-            {
-                lives = value;
-                L1.Visibility = (lives >= 1) ? Visibility.Visible : Visibility.Collapsed;
-                L2.Visibility = (lives >= 2) ? Visibility.Visible : Visibility.Collapsed;
-                L3.Visibility = (lives >= 3) ? Visibility.Visible : Visibility.Collapsed;
-                L4.Visibility = (lives >= 4) ? Visibility.Visible : Visibility.Collapsed;
-                L5.Visibility = (lives >= 5) ? Visibility.Visible : Visibility.Collapsed;                
-            }
-        }
-
-
+        
     }
 }
